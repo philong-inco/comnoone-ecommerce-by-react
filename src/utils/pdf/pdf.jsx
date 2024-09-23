e23d186aeac26b97152964186a13ba84e7c0d104
manh_03 <nguyenmanh19924@gmail.com>
2024-09-23 22:35:55 +0700

    pdf

--- src/utils/pdf/pdf.jsx
+++ src/utils/pdf/pdf.jsx
@@ -4,7 +4,7 @@ function pdfForm() {
   return (
     <>
       <div ref={componentRef} style={{ width: '100%', height: window.innerHeight }}>
-        <h1>PDF FILE</h1>
+        <h1 className="text-center my-3 border py-2">PDF FILE</h1>
       </div>
     </>
   );
