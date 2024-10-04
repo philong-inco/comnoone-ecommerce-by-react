11672a68ac94cf3e71978b32adccd5b417df4302
DESKTOP-54BBH23\vutu8 <ewfngoctu@gmail.com>
2024-10-04 11:25:42 +0700

    NPM build

---
+++ dist/assets/index-CVU_pdoE.js
@@ -0,0 +1,1 @@
+import{r as s,j as e}from"./index-COCf9D-9.js";const o=()=>{const[r,t]=s.useState(!0);return s.useEffect(()=>{t(!1)},[]),e.jsx("div",{children:"NgocTu"})};export{o as default};
---
+++ dist/assets/notification-B40vY8uZ.js
@@ -0,0 +1,1 @@
+const t=Object.freeze({UPDATED:"Cập nhật thành công",CREATED:"Tạo mới thành công",DELETED:"Xóa thành công",ERROR:"Có lỗi gì đó xảy ra, vui lòng thử lại"});export{t as N};
