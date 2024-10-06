16185c790f7ed78125d5b27bf62ab6d795426487
DESKTOP-54BBH23\vutu8 <ewfngoctu@gmail.com>
2024-10-06 12:45:48 +0700

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. #

---
+++ dist/assets/Add-DZNXi8zC.js
@@ -0,0 +1,1 @@
+import{h,j as s}from"./index-DvRT1fKX.js";const o=h(s.jsx("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}),"Add");export{o as A};
---
+++ dist/assets/useThemeProps-z8at_mIM.js
@@ -0,0 +1,1 @@
+import{aP as s,aQ as r,aR as m}from"./index-DvRT1fKX.js";function t({props:e,name:a}){return s({props:e,name:a,defaultTheme:r,themeId:m})}export{t as u};
