d221023d67a90aac5f10eca94400bdf67f2be51e
DESKTOP-54BBH23\vutu8 <ewfngoctu@gmail.com>
2024-10-06 15:26:15 +0700

    Reload page

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,9 @@ const App = () => {
       <ThemeProvider theme={themes(customization)}>
         <CssBaseline />
         <NavigationScroll>
-          <BrowserRouter> 
+         
             <RouterProvider router={router} />
-          </BrowserRouter>
+          
         </NavigationScroll>
       </ThemeProvider>
     </StyledEngineProvider>
