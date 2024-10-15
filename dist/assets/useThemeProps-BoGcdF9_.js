7771fc60fab95d5e4cbae9a3729b9ff1a084ee75
DESKTOP-54BBH23\vutu8 <ewfngoctu@gmail.com>
2024-10-15 14:09:36 +0700

    after build

---
+++ dist/assets/useThemeProps-BoGcdF9_.js
@@ -0,0 +1,1 @@
+import{v as r,w as m,x as o}from"./index-BIssY3ES.js";function u({props:e,name:s}){return r({props:e,name:s,defaultTheme:m,themeId:o})}export{u};
--- dist/assets/useThemeProps-C91a8WKB.js
+++
@@ -1,1 +0,0 @@
-import{v as r,w as m,x as o}from"./index-DkjfH3Nj.js";function u({props:e,name:s}){return r({props:e,name:s,defaultTheme:m,themeId:o})}export{u};
